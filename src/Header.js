7031fd5b270f7393e4427f9d666e944245c139fd
mattbed <m.bednarski@gmail.com>
2022-05-30 22:33:18 -0400

    Set up basic structure

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+import React from "react";
+
+function Header() {
+  return (
+    <div></div>
+  );
+}
+
+export default Header;
---
+++ src/MainView.js
@@ -0,0 +1,10 @@
+import './MainView.css';
+import React from "react";
+
+function MainView() {
+  return (
+    <div></div>
+  );
+}
+
+export default MainView;
