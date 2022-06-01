4df5af6245e4f97db98545e957419e82de5a2969
mattbed <m.bednarski@gmail.com>
2022-06-01 01:09:57 -0400

    Implement basic home page layout

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,14 @@ import React from "react";
 
 function Home() {
   return (
-    <div></div>
+    <main class="outer-box">
+      <div class="text-box-01">My name is</div>
+      <div class="text-box-02">Maciej</div>
+      <div class="text-box-03">but people call me</div>
+      <div class="text-box-04">Matthew</div>
+      <div class="text-box-05">I'm a Full Stack Web Developer</div>
+      <div class="text-box-06">and this is my website</div>
+    </main>
   );
 }
 
